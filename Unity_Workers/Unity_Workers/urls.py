943e64f9ff3a6f5808c9e2c3bdae862aa44d7b17
Akshay <akshaychoudhary2040@gmail.com>
2024-10-21 20:52:37 +0530

    80% fornt-end completed

--- Unity_Workers/Unity_Workers/urls.py
+++ Unity_Workers/Unity_Workers/urls.py
@@ -19,5 +19,12 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home')
+    path('',views.language_popup,name='language_popup'),
+    path('client_form/',views.client_form,name='client_form'),
+    path('client_mobile_verification',views.client_mobile_verification,name='client_mobile_verification'),
+    path('client_otp_verification',views.client_otp_verification,name='client_otp>verification.html'),
+    path('client_payment',views.client_payment,name='client_payment'),
+    path('home_page',views.home_page,name='home_page'),
+    path('worker_login_page',views.worker_login_page,name='worker_login_page'),
+    path('worker_registration_form',views.worker_registration_form,name='worker_registration_form')
 ]
--- Unity_Workers/Unity_Workers/views.py
+++ Unity_Workers/Unity_Workers/views.py
@@ -3,5 +3,28 @@ from django.shortcuts import render
 
 
 
-def home(request):
-    return render(request,'sample.html')
+def language_popup(request):
+    return render(request,'language_popup.html')
+
+
+def client_form(request):
+    return render(request,'client_form.html')
+
+
+def client_mobile_verification(request):
+    return render(request,'client_mobile_verification.html')
+
+def client_otp_verification(request):
+    return render(request,'client_otp_verification.html')
+
+def client_payment(request):
+    return render(request,'client_payment.html')
+
+def home_page(request):
+    return render(request,'home_page.html')
+
+def worker_login_page(request):
+    return render(request,'worker_login_page.html')
+
+def worker_registration_form(request):
+    return render(request,'worker_registration_form.html')
